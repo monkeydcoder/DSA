8b8b9660f40c2eda313d84b584a74a5887a28ac4
Satyam <satyamsharma3002@gmail.com>
2023-03-11 12:18:55 +0530

    code for solid rectangle pattern, takeing side as input

--- Patterns/solid_rectangle.cpp
+++ Patterns/solid_rectangle.cpp
@@ -4,17 +4,16 @@ using namespace std;
 int main()
 {
     // Printing a solid rectangle of given side 
-    int side;
-    cout << "Enter the number of side " << endl;
-    cin >> side
-    for (int row = 0; row < side; row++)
+    int num_row, num_col;
+    cout  << "Enter the number of row's and column's " << endl;
+    cin >> num_row >> num_col;
+    for (int row = 0; row < num_row; row++)
     {
-        for (int col = 0; col < side; col++)
+        for (int col = 0; col < num_col; col++)
         {
             cout << "* ";
         }
         cout << endl;
     }
-    
     return 0;
 }
