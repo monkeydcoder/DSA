1c5283e2e920d6cefc8bc5d5328335b4b5b7575d
Satyam <satyamsharma3002@gmail.com>
2023-05-22 22:07:47 +0530

    Remove all occurence of a substring

---
+++ leetcode/Remove_occ_of_string.cpp
@@ -0,0 +1,15 @@
+// Code for leet code problem named as : Remove all Occurence of a string 
+// link :https://leetcode.com/problems/remove-all-occurrences-of-a-substring/description/
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int pos = s.find(part);
+        while(pos != string::npos)
+        {
+            s.erase(pos, part.length());
+            pos = s.find(part);
+        }
+        return s;
+    }
+};
