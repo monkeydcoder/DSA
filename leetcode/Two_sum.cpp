013e71a0422ba804bff409443d1e80485c58122f
Satyam <satyamsharma3002@gmail.com>
2023-05-23 15:50:13 +0530

    Leet code easy

---
+++ leetcode/Two_sum.cpp
@@ -0,0 +1,20 @@
+// Solution of leetcode problem named as Two sum
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        for(int i=0; i<nums.size(); i++)
+        {
+            for(int j=i+1; j<nums.size(); j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                return {i, j};
+                break;
+                }
+            }
+        }
+        return {};
+    }
+};
